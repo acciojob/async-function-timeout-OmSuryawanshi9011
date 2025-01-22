11a2add71eada73c56adc54f24d4498562f6e42d
OmSuryawanshi9011 <131766249+OmSuryawanshi9011@users.noreply.github.com>
2025-01-22 16:04:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+document.getElementById('btn').addEventListener('click',async function() {
+	var text = document.getElementById('text').value;
+	var delay = document.getElementById('delay').value;
+
+	await new Promise(resolve => setTimeout(resolve, delay));
+
+	document.getElementById('output').innerText = text;
+});
